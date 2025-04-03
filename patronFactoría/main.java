c4d51f09c5ff2251d215a4181d83b5571568ab4e
mapc-o <151883869+mapc-o@users.noreply.github.com>
2025-04-02 20:26:57 -0600

    Add files via upload

---
+++ patronFactoría/main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import productos.Tienda;
+import productos.factory.productoFactory;
+import productos.factory.Producto;
+
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Librería en línea\n\n");
+        Scanner key = new Scanner(System.in);
+        int opcion = 0;
+        Tienda lib = new Tienda();
+        lib.menu();
+        opcion = key.nextInt();
+
+     
+        Producto producto = productoFactory.getProducto(opcion);
+        System.out.println(producto);
+
+       
+    }
+    
+}
