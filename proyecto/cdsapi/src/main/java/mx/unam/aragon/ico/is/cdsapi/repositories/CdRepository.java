5c7702c6714789db40569cd126f9191a6330dbaf
mapc-o <151883869+mapc-o@users.noreply.github.com>
2025-06-03 18:29:15 -0600

    Delete proyecto/cdsapi directory

--- proyecto/cdsapi/src/main/java/mx/unam/aragon/ico/is/cdsapi/repositories/CdRepository.java
+++
@@ -1,9 +0,0 @@
-package mx.unam.aragon.ico.is.cdsapi.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import mx.unam.aragon.ico.is.cdsapi.entities.Cd;
-
-public interface CdRepository extends CrudRepository<Cd, Long> {
-    public Cd findCdByClave(Long Clave);
-    public Cd deleteByClave(Long Clave);
-}
